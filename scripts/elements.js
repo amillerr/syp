a8a6b8296d9ab6ef9c48dd2e1aa3d04ae59414d2
Artem Miller <artem.millerr@yandex.ru>
2020-07-02 10:01:39 +0300

    feat: сверстал попапы

---
+++ scripts/elements.js
@@ -0,0 +1,28 @@
+// Подгружаем карточки
+
+const initialElements = [
+    {
+        name: 'Изельтвальд, Швейцария',
+        link: 'https://c.radikal.ru/c35/2007/c5/e524155a627b.jpg'
+    },
+    {
+        name: 'Нордфьорд, Норвегия',
+        link: 'https://d.radikal.ru/d25/2007/81/19e2d8774bf6.jpg'
+    },
+    {
+        name: 'Брайес, Италия',
+        link: 'https://c.radikal.ru/c40/2007/2b/1124c465418d.jpg'
+    },
+    {
+        name: 'Майя Бэй, Тайланд',
+        link: 'https://c.radikal.ru/c04/2007/b3/e5dfcb88ee71.jpg'
+    },
+    {
+        name: 'Рейкьявик, Исландия',
+        link: 'https://a.radikal.ru/a29/2007/bd/b87e7fafa9c9.jpg'
+    },
+    {
+        name: 'Перито-Морено, Аргентина',
+        link: 'https://c.radikal.ru/c18/2007/6a/c1f3fcc3c624.jpg'
+    }
+]
--- scripts/script.js
+++ scripts/script.js
@@ -14,41 +14,12 @@ const newDescription = document.querySelector('.popup__input_type_description')
 // Переменные elements
 const placeList = document.querySelector('.elements__list')
 
-// Подгружаем карточки
-
-const initialElements = [
-	{
-		name: 'Изельтвальд, Швейцария',
-		link: 'https://c.radikal.ru/c35/2007/c5/e524155a627b.jpg'
-	},
-	{
-		name: 'Нордфьорд, Норвегия',
-		link: 'https://d.radikal.ru/d25/2007/81/19e2d8774bf6.jpg'
-	},
-	{
-		name: 'Брайес, Италия',
-		link: 'https://c.radikal.ru/c40/2007/2b/1124c465418d.jpg'
-	},
-	{
-		name: 'Майя Бэй, Тайланд',
-		link: 'https://c.radikal.ru/c04/2007/b3/e5dfcb88ee71.jpg'
-	},
-	{
-		name: 'Рейкьявик, Исландия',
-		link: 'https://a.radikal.ru/a29/2007/bd/b87e7fafa9c9.jpg'
-	},
-	{
-		name: 'Перито-Морено, Аргентина',
-		link: 'https://c.radikal.ru/c18/2007/6a/c1f3fcc3c624.jpg'
-	}
-]
-
 // Выводим карточки 
 
 function showPlace(elem) {
 	const placeTemplate = document.querySelector('#elements-template').content
 	const placeElement = placeTemplate.cloneNode(true)
-	console.log(elem)
+
 	placeElement.querySelector('.element__img').src = elem.link
 	placeElement.querySelector('.element__name').textContent = elem.name
 
